0d446b146ad6231f7567bfacc301d90bfbfca0ca
ryanda9910 <ryandaaldo@gmail.com>
2022-03-12 11:19:16 +0700

    solve find_the_symetric_difference

--- freecodecamp/coding_interview_prep/algorithms/find_the_symetric_difference.js
+++ freecodecamp/coding_interview_prep/algorithms/find_the_symetric_difference.js
@@ -1,5 +1,35 @@
-function sym(args) {
-  return args;
+
+/* 
+ return array {number[]}
+*/
+
+// First you need to find complement arr1 and arr 2
+let loopArr = (arr1, arr2) => {
+  let answerArr = []
+  for (let i = 0; i < arr1.length; i++) {
+    let find1 = arr2.includes(arr1[i])
+    if (!find1) {
+      answerArr.push(arr1[i]) // will return array that complement to arr1
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    let find2 = arr1.includes(arr2[j])
+    if (!find2) {
+      answerArr.push(arr2[j]) // will return array that complement to arr2
+    }
+  }
+  return answerArr;
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+
+
+/* 
+ return set complement {number[]}
+*/
+function sym(...args) {
+  return ([...new Set(args.reduce(loopArr))])  // set new array and validate every single part inside array doesn't have same value
+}
+
+sym([1, 2, 5], [2, 3, 5], [3, 4, 5])
+
