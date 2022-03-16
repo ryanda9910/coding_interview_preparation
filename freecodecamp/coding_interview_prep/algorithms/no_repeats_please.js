4f6785f9fbd4ccb6e6e97c62e06cfce5808e4c5f
hendriyono <hendriyono97@gmail.com>
2022-03-16 20:03:34 +0700

    solve pairwise make pairwise.md

--- freecodecamp/coding_interview_prep/algorithms/no_repeats_please.js
+++ freecodecamp/coding_interview_prep/algorithms/no_repeats_please.js
@@ -1,5 +1,70 @@
 function permAlone(str) {
-  return str;
+  // Create a regex to match repeated consecutive characters.
+  let regex = /(.)\1+/;
+
+  // Split the string into an array of characters.
+  const arr = str.split("");
+  const permutations = [];
+  let tmp;
+
+  // console.log(arr);
+  // console.log([0])
+  // console.log(str.match(regex))
+  // console.log(str.match(regex)[0])
+  // console.log(str)
+
+  // Return 0 if str contains same character.
+  if (str.match(regex) !== null && str.match(regex)[0] === str) return 0;
+
+  // console.log(str)
+
+  // Function to swap variables' content.
+  function swap(index1, index2) {
+    tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+  }
+
+  // Generate arrays of permutations using the algorithm.
+  function generate(int) {
+    console.log(int)
+    if (int === 1) {
+      // Make sure to join the characters as we create  the permutation arrays
+      permutations.push(arr.join(""));
+    } else {
+      for (let i = 0; i != int; ++i) {
+        generate(int - 1);
+        swap(int % 2 ? 0 : i, int - 1);
+      }
+    }
+  }
+
+  generate(arr.length);
+
+  // Filter the array of repeated permutations.
+  const filtered = permutations.filter(function(string) {
+    return !string.match(regex);
+  });
+  console.log(filtered)
+
+  // console.log(filtered)
+
+  // Return how many have no repetitions.
+  return filtered.length;
+
 }
 
-permAlone('aab');
+permAlone('aaa');
+
+let datas =  [2,3,45]
+
+let test = (datas) => {
+  for(let i = 0; i < datas.length; i++){
+    console.log(datas[i])
+  }
+}
+
+test(datas)
+
+
+
--- freecodecamp/coding_interview_prep/algorithms/pairwise.js
+++ freecodecamp/coding_interview_prep/algorithms/pairwise.js
@@ -1,5 +1,30 @@
 function pairwise(arr, arg) {
-  return arg;
+  let pairIndices = [];
+  // Check every pair
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(arr[i])
+      console.log(arr[j])
+      console.log(arg)
+      console.log(i)
+      console.log("==================")
+      
+      // Exclude pairs that contain previously paired elements
+      if (arr[i] + arr[j] == arg
+          && !pairIndices.includes(i)
+          && !pairIndices.includes(j)) {
+          pairIndices.push(i, j);
+        break;
+      }
+    }
+  }
+
+  console.log(pairIndices); 
+
+  // console.log(pairIndices);
+  let result = pairIndices.reduce((sum, curr, index) => sum + curr, 0);
+  // console.log(result)
+  return result;
 }
 
-pairwise([1,4,2,3,0,5], 7);
+console.log(pairwise([1,4,2,3,0,5], 7));
