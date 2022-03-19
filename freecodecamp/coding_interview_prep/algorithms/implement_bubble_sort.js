86cae933c3b423c54569c951042969f64aef8cc9
ryanda9910 <ryandaaldo@gmail.com>
2022-03-19 17:08:41 +0700

    solve implement_bubble_sort

--- freecodecamp/coding_interview_prep/algorithms/implement_bubble_sort.js
+++ freecodecamp/coding_interview_prep/algorithms/implement_bubble_sort.js
@@ -1,5 +1,21 @@
+/** 
+* @param {number[]}
+* @returns {number[]}
+* 
+*/
+
 function bubbleSort(array) {
-  // Only change code below this line
+  for(let i = 0; i < array.length; i++){ // compare each element to the one 
+    for(let j = 0;  j < array.length-i ; j++){
+      if(array[j] > array[j+1]){ // if smaller swap the  value beetween index 
+       let temp = array[j]
+       array[j] = array[j+1]
+       array[j+1] = temp
+      }
+    }
+  }
   return array;
-  // Only change code above this line
+
 }
+
+bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])
